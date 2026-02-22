a6a199d21a41f85409ea1688817c9c8fd87d1c86
Nuimi <Nuimi@seznam.cz>
2026-02-22 15:09:52 +0100

    data fixes

--- src/api/database/registrations.js
+++ src/api/database/registrations.js
@@ -2,12 +2,12 @@ export function getRegistrationData()
 {
     return  [
         {
-            animeId: 'e1',
+            examId: 'e1',
             userId: 'student-1',
             status: 'REGISTERED'
         },
         {
-            animeId: 'e1',
+            examId: 'e1',
             userId: 'student-2',
             status: 'REGISTERED'
         }
