4d6f8f5a038efa777476a0747635e38a97017adc
Nuimi <Nuimi@seznam.cz>
2026-02-22 16:00:53 +0100

    layout fixes

--- src/ui/builder/layout/cardSmall.js
+++ src/ui/builder/layout/cardSmall.js
@@ -3,7 +3,7 @@ import { createText } from "../components/text.js";
 import { createDiv } from "../components/div.js";
 import { createArticle } from "../components/article.js";
 
-export function createCard({ title, description})
+export function createCard({ title, description, button = ''})
 {
     const titleElement = createTitle(2, title);
     const textElement1 = createText( description);
@@ -11,5 +11,5 @@ export function createCard({ title, description})
     const cardTitle = createDiv('card__title card__title--small', [titleElement]);
     const cardText = createDiv('card__text card__text--small bold', [textElement1]);
 
-    return createArticle( "card" , [cardTitle, cardText]  );
+    return createArticle( "card" , [cardTitle, cardText, button]  );
 }
